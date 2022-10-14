1c5b5416912692e9a0290dc1085eee24c876945b
Adam Mikó <okukster@gmail.com>
2022-10-14 15:37:16 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const App: FC = (): JSX.Element => {
+	return <div>https://www.typescriptlang.org/</div>;
+};
+
+export default App;
---
+++ src/playground/types.ts
@@ -0,0 +1,10 @@
+/**
+ * Primitivne typy
+ */
+
+const userName: string = 'Adam';
+
+type UserName2 = string;
+const userName2: UserName2 = 'Adam'; //
+const userName3: UserName2 = 12;
+const userName4: UserName2 = {};
